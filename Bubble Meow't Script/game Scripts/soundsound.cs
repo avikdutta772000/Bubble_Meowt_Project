f2ade010cfdfcc220c9b34e7345490dd2a5e8f97
avikdutta2000 <68175543+avikdutta2000@users.noreply.github.com>
2021-06-30 13:20:48 +0530

    script add

---
+++ Bubble Meow't Script/game Scripts/soundsound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSound : MonoBehaviour
+{
+  
+    void StayAwake()
+    {
+       
+            DontDestroyOnLoad(transform.gameObject);
+         
+    }
+    
+}
